3d3e90d10459cdf8c7605e5ad1994220c1992eb5
fjork3 <horkley@superconductive.com>
2021-12-02 10:09:47 -0500

    day 2

--- 2021/scratch.py
+++ 2021/scratch.py
@@ -5,5 +5,26 @@ def puzzle1(windowsize: int) -> int:
         diffs = [depths[i+windowsize] - depths[i] for i in range(len(depths)-windowsize)]
         return sum([x > 0 for x in diffs])
 
-print(f"Part 1: {puzzle1(1)}")
-print(f"Part 2: {puzzle1(3)}")
+# print(f"Day 1, part 1: {puzzle1(1)}")
+# print(f"Day 1, part 2: {puzzle1(3)}")
+
+def puzzle2() -> int:
+    with open('input2.txt', 'r') as f:
+        commands = map(lambda x: x.split(" "), f.readlines())
+    x_pos = 0
+    y_pos = 0
+    aim = 0
+    for (dir, step) in commands:
+        step = int(step)
+        if dir == "forward":
+            x_pos += step
+            y_pos += step * aim
+        elif dir == "up":
+            aim -= step
+        elif dir == "down":
+            aim += step
+
+    return x_pos * y_pos
+
+
+print(f"Day 2: {puzzle2()}")
