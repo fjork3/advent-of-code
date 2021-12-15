08be05298e3d5f9cf3cdf1fcf55c2262eb126e79
fjork3 <horkley@superconductive.com>
2021-12-15 16:39:45 -0500

    day 14, part 2

--- 2021/day_14.py
+++ 2021/day_14.py
@@ -6,29 +6,41 @@ def read_input() -> Tuple[str, Dict]:
     with open("inputs/input14.txt", "r") as f:
         sequence = f.readline().rstrip()
         f.readline()
-        insertions = defaultdict(str)
+        insertions = {}
         while line := f.readline():
             pair, insert = line.rstrip().split(" -> ")
             insertions[pair] = insert
     return sequence, insertions
 
 
-def step(sequence: str, insertions: Dict):
+def step_brute_force(sequence: str, insertions: Dict):
     output = ""
     for i in range(len(sequence)):
         output += sequence[i]
-        output += insertions[
-            sequence[i : i + 2]
-        ]  # will insert empty string if not in listed pairs
-    # DEBUG
-    print(f"step complete! new polymer size: {len(output)}")
+        if sequence[i : i + 2] in insertions:
+            output += insertions[sequence[i : i + 2]]
     return output
 
 
+def step_pairs(insertions: Dict, counts: Counter) -> Counter:
+    # what happens when we do a pair insertion?
+    # - if it's in the substitution list, that count goes down by 1 and the overlap counts go up by 1
+    # - if it's not in the substitution list, no counts change
+    new_counts = counts.copy()
+    for pair in counts:
+        if pair in insertions:
+            n = counts[pair]
+            new_counts[pair[0] + insertions[pair]] += n
+            new_counts[insertions[pair] + pair[1]] += n
+            new_counts[pair] -= n
+    return new_counts
+
+
 def part_one():
+    # small enough to brute force quickly
     sequence, insertions = read_input()
     for i in range(10):
-        sequence = step(sequence, insertions)
+        sequence = step_brute_force(sequence, insertions)
     # most common element minus least common
     counts = Counter(sequence)
     return max(counts.values()) - min(counts.values())
@@ -36,10 +48,19 @@ def part_one():
 
 def part_two():
     sequence, insertions = read_input()
-    for i in range(40):
-        sequence = step(sequence, insertions)
-    # most common element minus least common
-    counts = Counter(sequence)
+
+    pair_counts = Counter(["".join(p) for p in zip(sequence, sequence[1:])])
+    for _ in range(40):
+        pair_counts = step_pairs(insertions, pair_counts)
+
+    # count only first letter of each pair, to avoid double-counting overlap
+    counts = defaultdict(int)
+    for pair in pair_counts:
+        counts[pair[0]] += pair_counts[pair]
+    # final letter isn't part of any pairs (any never changes), account for that
+    counts[sequence[-1]] += 1
+    print(counts)
+
     return max(counts.values()) - min(counts.values())
 
 
